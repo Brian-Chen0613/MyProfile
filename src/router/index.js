c8a95bf7ec5fb76cf4f0950550aa2dbebdf1d8a9
Brian-Chen0613 <xddxo14@gmail.com>
2026-06-29 10:48:27 +0800

    feat: 改為hash mode模式

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import MyProfile from "@/views/MyProfile.vue";
 import Skills from "@/views/Skills.vue";
 import Projects from "@/views/Projects.vue";
 import ProjectsDetail from "@/views/ProjectsDetail.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   scrollBehavior() {
     return { top: 0 };
   },
